225bd161be1633e43eb6ef8cdfa8b528dad2faab
winand <winand1990@live.nl>
2023-02-13 23:27:25 +0100

    Updated the versioning of Quilt and renamed packages etc

--- src/main/java/dev/we_drink_tea/nekromantia/mixin/TitleScreenMixin.java
+++ src/main/java/dev/we_drink_tea/nekromantia/mixin/TitleScreenMixin.java
@@ -1,7 +1,7 @@
-package com.example.example_mod.mixin;
+package dev.we_drink_tea.nekromantia.mixin;
 
 import net.minecraft.client.gui.screen.TitleScreen;
-import com.example.example_mod.ExampleMod;
+import dev.we_drink_tea.nekromantia.Nekromantia;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
 	@Inject(method = "init", at = @At("TAIL"))
-	public void exampleMod$onInit(CallbackInfo ci) {
-		ExampleMod.INSTANCE.getLOGGER().info("This line is printed by an example mod mixin!");
+	public void nekromantia$onInit(CallbackInfo ci) {
+		Nekromantia.INSTANCE.getLOGGER().info("This line is printed by an example mod mixin!");
 	}
 }
--- src/main/kotlin/dev/we_drink_tea/nekromantia/Nekromantia.kt
+++ src/main/kotlin/dev/we_drink_tea/nekromantia/Nekromantia.kt
@@ -1,11 +1,11 @@
-package com.example.example_mod
+package dev.we_drink_tea.nekromantia
 
 import org.quiltmc.loader.api.ModContainer
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-object ExampleMod : ModInitializer {
+object Nekromantia : ModInitializer {
     val LOGGER: Logger = LoggerFactory.getLogger("Example Mod")
 
     override fun onInitialize(mod: ModContainer) {
